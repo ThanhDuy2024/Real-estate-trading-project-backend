8d40d523e2656097eb698c18e103bf04ca380f95
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-06-26 08:43:07 +0700

    first commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+import 'dotenv/config'
+import express from "express";
+
+const port = String(process.env.PORT);
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
