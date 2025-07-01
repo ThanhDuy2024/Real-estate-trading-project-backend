92e03598bee2e947e45a5768599f56ff94a5042c
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-07-01 20:37:21 +0700

    [Feature] admin accounts register

--- controllers/admin/accountAdmin.controller.ts
+++ controllers/admin/accountAdmin.controller.ts
@@ -1,8 +1,16 @@
 import { Request, Response } from "express";
-
+import bcrypt from "bcryptjs";
+import AccountAdmin from "../../models/accountAdmin.model";
 export const registerAdmin = async (req: Request, res: Response) => {
-  console.log(req.body);
-  res.json({
-    code: "success"
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(req.body.password, salt);
+  req.body.password = hash;
+
+  const newRecord = new AccountAdmin(req.body);
+
+  await newRecord.save();
+  res.json({  
+    code: "success",
+    message: "Đăng ký thành công",
   });
 }
