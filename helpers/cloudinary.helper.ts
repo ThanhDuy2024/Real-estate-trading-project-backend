f1e683728aef3eaf6d0d81e378419f268f1e99fe
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-07-03 16:37:24 +0700

    [Helpers] cloudinary

---
+++ helpers/cloudinary.helper.ts
@@ -0,0 +1,13 @@
+import {v2 as cloudinary} from "cloudinary"
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import 'dotenv/config';
+
+cloudinary.config({
+  cloud_name: String(process.env.CLOUD_NAME),
+  api_key: String(process.env.API_KEY),
+  api_secret: String(process.env.API_SECRET)
+});
+
+export const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+});
