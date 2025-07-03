e70c92cbdde8051d1c1e2e50444e86dd10c1fb82
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-07-03 14:37:16 +0700

    [Validate] accounts login and accounts register

--- routes/admin/accountAdmin.route.ts
+++ routes/admin/accountAdmin.route.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import * as accountAdminController from "../../controllers/admin/accountAdmin.controller"; 
+import * as authValidate from "../../validates/auth.validate";
 const router = Router();
 
-router.post('/register', accountAdminController.registerAdmin);
+router.post('/register', authValidate.registerValidate, accountAdminController.registerAdmin);
 
-router.post('/login', accountAdminController.login)
+router.post('/login', authValidate.loginValidate, accountAdminController.login)
 export default router;
---
+++ validates/auth.validate.ts
@@ -0,0 +1,64 @@
+import { NextFunction, Request, Response } from "express";
+import Joi from "joi";
+
+export const registerValidate = (req: Request, res: Response, next: NextFunction) => {
+  const schema = Joi.object({
+    fullName: Joi.string().min(6).max(50).required()
+      .messages({
+        "string.empty": "full name is not empty",
+        "string.min": "full name must be at least 6 characters",
+        "string.max": "full name must be at highest 50 characters"
+      }),
+    email: Joi.string().email().required()
+      .messages({
+        "string.empty": "email is not empty",
+        "string.email": "email invalid",
+      }),
+    password: Joi.string().min(6).max(50).required()
+      .messages({
+        "string.empty": "password is not empty",
+        "string.min": "password must be least 6 characters",
+        "string.max": "password must be highest 50 characters",
+      })
+  })
+
+  const { error } = schema.validate(req.body);
+
+  if(error) {
+    res.json({
+      code: "error",
+      message: error.details[0].message,
+    })
+    return;
+  }
+
+  next();
+}
+
+export const loginValidate = (req: Request, res: Response, next: NextFunction) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required()
+      .messages({
+        "string.empty": "email is empty",
+        "string.email": "email invalid",
+      }),
+    password: Joi.string().min(6).max(50).required()
+      .messages({
+        "string.empty": "password is empty",
+        "string.min": "password must be least 6 characters",
+        "string.max": "password must be highest 50 characters",
+      })
+  })
+
+  const { error } = schema.validate(req.body);
+
+  if(error) {
+    res.json({
+      code: "error",
+      message: error.details[0].message,
+    })
+    return;
+  }
+
+  next();
+}
