35912f869c3cdc1f32331f04de52e170c11f09e8
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-07-19 20:45:09 +0700

    [Model] roles admin

---
+++ models/roles.model.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+const slug = require("mongoose-slug-updater");
+const { Schema } = mongoose;
+mongoose.plugin(slug);
+
+const schema = new Schema({
+  name: String,
+  permissions: Array,
+  status: String,
+  deleted: {
+    type: Boolean,
+    default: false
+  },
+  deletedAt: Date,
+  deletedBy: String,
+  updatedBy: String,
+  createdBy: String,
+  slug: {
+    type: String,
+    default: "name"
+  }
+}, {
+  timestamps: true
+})
+
+export const Role = mongoose.model("Role", schema, "roles");
