541f0ef14fd931670dd4f250e1cae7215f78ff51
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-07-29 20:44:50 +0700

    [Model] building

---
+++ models/building.model.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+const slug = require("mongoose-slug-updater");
+mongoose.plugin(slug);
+const { Schema } = mongoose;
+
+const schema = new Schema({
+  name: String,
+  avatar: String,
+  address: String,
+  categoryId: String,
+  acreage: Number, //dien tich
+  numberOfFloors: Number, //so tang cua can nha
+  rentPrice: Number,
+  purchasePrice: Number, 
+  status: String,
+  deleted: {
+    type: Boolean,
+    default: false,
+  },
+  deletedAt: Date,
+  deletedBy: String,
+  createdBy: String,
+  updatedBy: String,
+}, {
+  timestamps: true
+});
+
+const Building = mongoose.model("Building", schema, "building");
+
+export default Building;
