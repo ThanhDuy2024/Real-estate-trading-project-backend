22b7434403401a81e7b8e310cc70f2a1bbd4582e
Lâm Thành Duy <thanhduythptthanhbinh@gmail.com>
2025-08-07 08:53:24 +0700

    [Update] profile account admin, add object roleInfor, in object roleInfor have key roleId and roleName

--- controllers/admin/authentication.controller.ts
+++ controllers/admin/authentication.controller.ts
@@ -1,15 +1,34 @@
 import { Response } from "express";
 import { accountAdmin } from "../../interfaces/accountAdmin.interface";
+import { Role } from "../../models/roles.model";
 export const profile = async (req: accountAdmin, res: Response) => {
-  const dataFinal = {
+  const dataFinal: any = {
     id: req.accountAdmin._id,
     fullName: req.accountAdmin.fullName,
     email: req.accountAdmin.email,
     avatar: req.accountAdmin.avatar ? req.accountAdmin.avatar : "",
     phone: req.accountAdmin.phone ? req.accountAdmin.phone : "",
     address: req.accountAdmin.address ? req.accountAdmin.address : "",
-    roleId: req.accountAdmin.roleId ? req.accountAdmin.roleId : "",
+    roleInfor: ""
   }
+
+  const roleQuery = await Role.findOne({
+    _id: req.accountAdmin.roleId,
+    deleted: false
+  })
+
+  if (roleQuery) {
+    dataFinal.roleInfor = {
+      roleId: req.accountAdmin.roleId,
+      roleName: roleQuery.name
+    }
+  } else {
+    dataFinal.roleInfor = {
+      roleId: "",
+      roleName: ""
+    }
+  }
+
   res.json({
     code: "success",
     profile: dataFinal
